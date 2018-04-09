3b3642d5671beda0258c28140049ccf9955e6bda
Zach Leslie <xaque208@gmail.com>
2018-04-08 21:05:20 -0700

    Initial

---
+++ rftoy/rftoy.go
@@ -0,0 +1,41 @@
+package rftoy
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"strconv"
+)
+
+type RFToy struct {
+	Address string
+}
+
+type state bool
+
+func (t *RFToy) call(sid int, turn string) {
+	addr := fmt.Sprintf("%s/cc", t.Address)
+
+	req, err := http.NewRequest("GET", addr, nil)
+	q := req.URL.Query()
+	q.Add("sid", strconv.Itoa(sid))
+	q.Add("turn", turn)
+
+	if err != nil {
+		log.Error(err)
+	}
+
+	client := &http.Client{}
+
+	req.URL.RawQuery = q.Encode()
+	fmt.Println(req.URL.String())
+	client.Do(req)
+}
+
+func (t *RFToy) On(sid int) {
+	t.call(sid, "on")
+}
+
+func (t *RFToy) Off(sid int) {
+	t.call(sid, "off")
+}
